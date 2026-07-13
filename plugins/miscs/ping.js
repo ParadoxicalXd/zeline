395a6372125441a8dd57fe793ee0b2d5fcc12a31
ParadoxicalXd <uzairafzal496@gmail.com>
2026-07-13 18:20:51 +0500

    Update ping.js

--- plugins/miscs/ping.js
+++ plugins/miscs/ping.js
@@ -1,32 +1,14 @@
-import os from 'os'
-
 export const run = {
   cmd: ['ping'],
   hidden: ['p'],
   category: 'miscs',
-  description: 'show server status',
+  description: 'show bot status',
   run: async (m, { sock }) => {
 
-    const totalMem = os.totalmem()
-    const freeMem = os.freemem()
-    const usedMem = totalMem - freeMem
-    const memPercent = ((usedMem / totalMem) * 100).toFixed(1)
+    const cap = `🏓 *Pong!*
 
-    const cap = `\`Server Information\`
-* Running On : ${process.env.USER === "root" ? "VPS" : "HOSTING (PANEL)"}
-* Cwd : ${process.cwd()}
-* Hostname : ${os.hostname()}
-* Node Version : ${process.version}
-
-\`Management Server\`
-* Bot Speed : ${Date.now() - m.timestamps} ms
-* Uptime Bot : ${Func.toDate(process.uptime())}
-* Uptime Server : ${Func.toDate(os.uptime())}
-* Memory : ${Func.size(usedMem)} / ${Func.size(totalMem)} (${memPercent}%)
-* CPU : ${os.cpus()[0].model} ( ${os.cpus().length} CORE )
-* Release : ${os.release()}
-* Type : ${os.type()}
-* Arch : ${os.arch()}`
+*Bot Speed :* ${Date.now() - m.timestamps} ms
+*Uptime Bot :* ${Func.toDate(process.uptime())}`
 
     await m.reply(cap)
   }
